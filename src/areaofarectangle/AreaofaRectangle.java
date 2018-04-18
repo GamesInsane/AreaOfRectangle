8b0f185ab01524843ae23d0df8c1355d100314e3
thhoo7460 <thhoo7460@7KWN6Y1.Student.UGDSB.ED>
2018-04-18 13:30:24 -0400

    done

---
+++ src/areaofarectangle/AreaofaRectangle.java
@@ -0,0 +1,26 @@
+/*
+ *Tom Hoogkamp
+ *AreaofaRectangle.java
+ *27 February, 2018
+ */
+
+package areaofarectangle;
+
+/**
+ *
+ * @author thhoo7460
+ */
+public class AreaofaRectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello there");
+        double length = 5.7;
+        double width = 4.8;
+        double areaRectangle = length * width;
+        System.out.println("the area of the rectangle is "+ areaRectangle);
+    }
+    
+}
